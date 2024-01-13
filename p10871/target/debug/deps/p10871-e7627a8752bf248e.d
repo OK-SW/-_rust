c17039a64c17b0b21f58f79e7160249b0d2a41ac
Seonwoo Ok <144411938+OK-SW@users.noreply.github.com>
2024-01-13 16:34:50 +0900

    Add files via upload

---
+++ p10871/target/debug/deps/p10871-e7627a8752bf248e.d
@@ -0,0 +1,5 @@
+/Users/ok-sw/ok/onlinejudge/p10871/target/debug/deps/p10871-e7627a8752bf248e: src/main.rs
+
+/Users/ok-sw/ok/onlinejudge/p10871/target/debug/deps/p10871-e7627a8752bf248e.d: src/main.rs
+
+src/main.rs:
---
+++ p10871/target/debug/p10871.d
@@ -0,0 +1,1 @@
+/Users/ok-sw/ok/onlinejudge/p10871/target/debug/p10871: /Users/ok-sw/ok/onlinejudge/p10871/src/main.rs
